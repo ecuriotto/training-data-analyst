4cf03728726f168381db147e9917a4b16acc4356
Enrico <enrico.curiotto@bonitasoft.com>
2020-05-05 18:20:33 +0200

    updated Image service for storage

--- courses/developingapps/v1.2/java/setjava8.sh
+++ courses/developingapps/v1.2/java/setjava8.sh
@@ -3,6 +3,6 @@ sudo apt-get update
 sudo apt-get install openjdk-8-jdk
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 echo "Running update-alternatives to set java 8 instead of 11"
-sudo update-alternatives --install /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 2
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 2
 java -version
 echo $JAVA_HOME
